88a3e71276f8ea161c93cefb36fd2ebe8da07189
Jordan <jhimpinfo@gmail.com>
2025-11-07 14:28:29 -0500

    Fix connection

--- mbw/src/dbConfig/db.js
+++ mbw/src/dbConfig/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-const { MONGODB_URI } = process.env;
-if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+const { MONGODB_URL } = process.env;
+if (!MONGODB_URL) throw new Error("MONGODB_URI is missing");
 
 let cached = global._mongoose || { conn: null, promise: null };
 
 export async function connectToDB() {
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, { serverSelectionTimeoutMS: 5000 });
+    cached.promise = mongoose.connect(MONGODB_URL, { serverSelectionTimeoutMS: 5000 });
   }
   cached.conn = await cached.promise;
   global._mongoose = cached;
