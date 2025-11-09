938795d3492fccf4e17f9f325afd439b16306a5f
Jordan <jhimpinfo@gmail.com>
2025-11-09 13:38:55 -0500

    Render

--- mbw/src/dbConfig/db.js
+++ mbw/src/dbConfig/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 let cached = global._mongoose || { conn: null, promise: null };
 
 export async function connectToDB() {
-  const uri = process.env.MONGODB_URL;
+  const uri = process.env.MONGO_URL;
   if (!uri) throw new Error("MONGODB_URL is missing"); // inside function, not top level
   if (cached.conn) return cached.conn;
   if (!cached.promise) cached.promise = mongoose.connect(uri, { serverSelectionTimeoutMS: 5000 });
